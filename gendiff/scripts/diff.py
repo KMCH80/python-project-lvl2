e5b602202dda2a9482d79f1622cf03b733e07d71
Alexey Kim <kmlx@yandex.ru>
2021-05-18 00:12:53 +0300

    Changed Diff complexity

--- gendiff/scripts/diff.py
+++ gendiff/scripts/diff.py
@@ -4,18 +4,24 @@ def match_templates_old_items(template1: dict, template2: dict, diff: dict):
         for key2, value2 in template2.items():
             if key1 == key2:
                 flag = True
-                if value1 == value2:
-                    diff.update({key1: ""})
-                elif isinstance(value1, dict) and isinstance(value2, dict):
+                if isinstance(value1, dict) and isinstance(value2, dict):
                     diff.update({key1: {}})
                     match_templates_old_items(value1, value2, diff[key1])
                 else:
-                    diff.update({key1: "changed"})
+                    diff.update(
+                        {key1: get_change_or_not_status(value1, value2)})
                 break
         if not flag:
             diff.update({key1: "deleted"})
     return diff
 
+
+def get_change_or_not_status(value1, value2):
+    if value1 == value2:
+        status = ""
+    else:
+        status = "changed"
+    return status
 
 def match_templates_new(template1: dict, template2: dict, diff: dict):
     for key1, value1 in template2.items():
