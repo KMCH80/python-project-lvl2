a35d039daf9ee6c157d7c417d88430536bc1fa99
Alexey Kim <kmlx@yandex.ru>
2021-06-29 22:58:53 +0300

    Corrections

--- tests/test_plain_formater.py
+++ tests/test_plain_formater.py
@@ -5,9 +5,6 @@ from gendiff import generate_diff
 @pytest.mark.parametrize("file1,file2,result_file", [
     ('tests/fixtures/test6_tree_file1.json',
      'tests/fixtures/test6_tree_file2.json',
-     'tests/fixtures/test7_plain_res.txt'),
-    ('files/file4_1.yml',
-     'files/file4_2.yml',
      'tests/fixtures/test7_plain_res.txt')
 ])
 def test_diff(file1, file2, result_file):
