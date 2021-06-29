f86c113fcd154dbc4d40f17ad69950ac28e85611
Alexey Kim <kmlx@yandex.ru>
2021-06-29 22:52:36 +0300

    Corrections

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -27,7 +27,7 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file', help='')
     parser.add_argument('second_file', help='')
-    parser.add_argument('-f', '--format', choices=['plain', 'json'],
+    parser.add_argument('-f', '--format', choices=['stylish', 'plain', 'json'],
                         default='stylish', help='set format of output')
     args = parser.parse_args()
     print(generate_diff(args.first_file, args.second_file, args.format))
