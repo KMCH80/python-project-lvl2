22401dfbb4e6f4db05ae159a8203d7420b92a233
Alexey Kim <kmlx@yandex.ru>
2021-06-29 22:47:50 +0300

    Corrections

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,7 +6,7 @@ from gendiff.formaters.json import json_formater
 from gendiff.diff import get_matched_data
 
 
-def generate_diff(file1, file2, format_name=0):
+def generate_diff(file1, file2, format_name='stylish'):
     try:
         data1 = file_reader.get_data_from_file(file1)
         data2 = file_reader.get_data_from_file(file2)
@@ -19,7 +19,7 @@ def generate_diff(file1, file2, format_name=0):
         return plain_formater(diff, diff_result)
     elif format_name == 'json':
         return json_formater(diff, diff_json_result)
-    else:
+    elif format_name == 'stylish':
         return stylish_formater(diff, diff_result)
 
 
@@ -28,7 +28,7 @@ def main():
     parser.add_argument('first_file', help='')
     parser.add_argument('second_file', help='')
     parser.add_argument('-f', '--format', choices=['plain', 'json'],
-                        help='set format of output')
+                        default='stylish', help='set format of output')
     args = parser.parse_args()
     print(generate_diff(args.first_file, args.second_file, args.format))
 
