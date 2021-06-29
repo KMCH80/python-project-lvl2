28bf733574c9e90f81435f7d0c623693128aa6ec
Alexey Kim <kmlx@yandex.ru>
2021-06-29 22:34:33 +0300

    Corrections

--- gendiff/file_reader.py
+++ gendiff/file_reader.py
@@ -6,10 +6,9 @@ import json
 def get_data_from_file(file):
     extention = os.path.splitext(file)[1]
     with open(file) as f:
-        if (extention.lower() == ".yml"):
+        if (extention.lower() == ".yml") or (extention.lower() == ".yaml"):
             return yaml.safe_load(f)
         elif (extention.lower() == ".json"):
             return json.load(f)
         else:
             return 0
-
