0e5627381fcd2edbaec8b41aaff2c3d2495ec52f
Alexey Kim <kmlx@yandex.ru>
2021-08-11 23:26:36 +0300

    Changed

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,3 +1,4 @@
-from gendiff import diff
+from gendiff.diff import generate_diff
+from gendiff.diff import STYLE_OPT_JSON, STYLE_OPT_PLAIN, STYLE_OPT_STYLISH
 
-__all__ = ['diff']
+__all__ = ['generate_diff', STYLE_OPT_JSON, STYLE_OPT_PLAIN, STYLE_OPT_STYLISH]
